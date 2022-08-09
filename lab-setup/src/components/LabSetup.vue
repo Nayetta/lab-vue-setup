92f4606930d2cfd45a7038ea9fe9a9813e711982
Nayetta <noemi.silvestre@gmail.com>
2022-08-09 17:02:32 +0200

    lab 1 vue done

---
+++ lab-setup/src/components/LabSetup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ something }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      something: "Hey you!!! WuTangClan!!!",
+    };
+  },
+};
+</script>
+
+<style></style>
